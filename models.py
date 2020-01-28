962d6d924a76aa84f5eb6344035c87b8bc9931ad
Sarah Nicholson <sarah.nicholson.13@ucl.ac.uk>
2020-01-28 16:46:18 +0000

    Initial files for start of the lecture

---
+++ models.py
@@ -0,0 +1,48 @@
+from app import db
+
+
+class Student(db.Model):
+    __tablename__ = 'students'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    email = db.Column(db.Text)
+    password = db.Column(db.Text)
+    grades = db.relationship('Grade', backref=db.backref('students', lazy=True))
+
+    def __repr__(self):
+        return '<Student {}>'.format(self.name)
+
+
+class Teacher(db.Model):
+    __tablename__ = 'teachers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    courses = db.relationship('Course', backref='teachers', lazy=True)
+
+    def __repr__(self):
+        return '<Teacher {}>'.format(self.name)
+
+
+class Course(db.Model):
+    __tablename__ = 'courses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'), nullable=False)
+    grades = db.relationship('Grade', backref=db.backref('courses', lazy=True))
+
+    def __repr__(self):
+        return '<Course {}>'.format(self.name)
+
+
+class Grade(db.Model):
+    __tablename__ = 'grades'
+
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False, primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False, primary_key=True)
+    grade = db.Column(db.Text)
+
+    def __repr__(self):
+        return '<Grade {}>'.format(self.grade)
